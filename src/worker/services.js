9b5e65736a44054efbab34f56e27b7f470506729
Terence <texzcorp@gmail.com>
2023-06-04 01:19:46 +0200

    Try to fix : category event chain

--- src/worker/services.js
+++ src/worker/services.js
@@ -4,70 +4,25 @@ export class TwitchServices {
   /*
    * Internal Helpers
    */
-  async #subProfilesInfo(usernames = []) {
-    const query = tw.buildQuery(usernames, 'login');
-    const response = await tw.apiRequest('https://api.twitch.tv/helix/users', query);
-
-    const profilesData = response.data;
-
-    if (general.isEmpty(profilesData)) {
-      return null;
-    }
-
-    // I need these data separate. ids for next actions, names for UI.
-    const ids = profilesData.map((profile) => profile.id);
-    const names = profilesData.map((profile) => profile.login);
-
-    return { ids, names };
-  }
-
-  async #followingsPerUser(id) {
+  async #streamersInCategory(categoryNames = []) {
     const result = [];
 
-    let active = true;
-    let paginationKey = '';
-    // Api has 100 items return limit.
-    // We are getting the full data chunk by chunk size of 100:
-    do {
-      const response = await tw.apiRequest('https://api.twitch.tv/helix/users/follows', `from_id=${id}&first=100&after=${paginationKey}`);
-      const followingsDataChunk = response.data;
+    for (let i = 0; i < categoryNames.length; i++) {
+      const response = await tw.apiRequest('https://api.twitch.tv/helix/streams', `game=${encodeURIComponent(categoryNames[i])}&first=100`);
 
-      if (general.isEmpty(followingsDataChunk)) {
-        return null;
+      const streamersData = response.data;
+
+      if (general.isEmpty(streamersData)) {
+        continue;
       }
 
-      // Save current chunk of received data:
-      followingsDataChunk.forEach((channel) => {
-        if (channel.to_login) result.push(channel.to_login);
+      // Saving:
+      streamersData.forEach((streamer) => {
+        if (streamer.user_login) {
+          result.push(streamer.user_login);
+        }
       });
-
-      // Is there any more data?
-      if (response?.pagination?.cursor) {
-        paginationKey = response.pagination.cursor;
-      } else {
-        active = false;
-      }
-    } while (active);
-
-    return result;
-  }
-
-  async #subLiveChannels(channels = []) {
-    const result = [];
-
-    const query = tw.buildQuery(channels, 'user_login');
-    const response = await tw.apiRequest('https://api.twitch.tv/helix/streams', query);
-
-    const liveChannelsData = response.data;
-
-    if (general.isEmpty(liveChannelsData)) {
-      return null;
     }
-
-    // Saving:
-    liveChannelsData.forEach((channel) => {
-      if (channel.user_login) result.push(channel.user_login);
-    });
 
     return result;
   }
@@ -75,9 +30,9 @@ export class TwitchServices {
   /*
    * Main Services
    */
-  async getProfilesInfo(usernames = []) {
+  async getProfilesInfo(categories = []) {
     // Set user input limit:
-    if (usernames.length > 500) {
+    if (categories.length > 500) {
       throw new Error('Input limit exceeded. The maximum limit is 500');
     }
 
@@ -85,11 +40,11 @@ export class TwitchServices {
     let names = [];
 
     // Chunk the user input size of 100:
-    const usernameChunks = general.chunkArray(usernames, 100);
+    const categoryChunks = general.chunkArray(categories, 100);
 
     // Load data chunk by chunk:
-    for (let i = 0; i < usernameChunks.length; i++) {
-      const subResult = await this.#subProfilesInfo(usernameChunks[i]);
+    for (let i = 0; i < categoryChunks.length; i++) {
+      const subResult = await this.#streamersInCategory(categoryChunks[i]);
 
       if (subResult === null) continue;
 
@@ -102,27 +57,6 @@ export class TwitchServices {
     }
 
     return { ids, names };
-  }
-
-  async getFollowings(ids = []) {
-    let channels = [];
-
-    // Load one by one:
-    for (let i = 0; i < ids.length; i++) {
-      const subResult = await this.#followingsPerUser(ids[i]);
-
-      if (subResult === null) continue;
-
-      channels = channels.concat(subResult);
-    }
-
-    if (general.isEmpty(channels)) {
-      throw new Error('No data found');
-    }
-
-    ui.unblockSpyButton();
-
-    return [...new Set(channels)];
   }
 
   async getLiveChannels(channels = []) {
